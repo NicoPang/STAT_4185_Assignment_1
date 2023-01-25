b12db8757d43d3f7999078459087a24fd9bc6638
Nick Pang <48924710+NicoPang@users.noreply.github.com>
2023-01-25 10:36:20 -0500

    Added decryption code.

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,10 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+split_string = [*encrypted_message]
+for i in range(1, len(split_string)//2 + 2, 2):
+    temp = split_string[i]
+    split_string[i] = split_string[i * -1 - 1]
+    split_string[i * -1 - 1] = temp
+
+print(''.join(split_string))
